593d5abc634363a7f298c40518a21492c887dfba
Sefaciftci <75899474+Sefaciftci@users.noreply.github.com>
2024-08-14 17:30:50 +0300

    link update

--- download-app/src/App.jsx
+++ download-app/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
             <img className='icon' src={ainosLogo}></img>
             <h3>Ainos Object Detection <span className='span'>V1.1</span> </h3>
             <div className='btn-div' >
-              <a href='https://drive.google.com/uc?export=download&id=1lZvDcHzx1t9jMx4R55al6yFQN9-0LaEg
+              <a href='https://drive.google.com/file/d/1lZvDcHzx1t9jMx4R55al6yFQN9-0LaEg/view?usp=sharing
 ' className='download-btn'>Download</a>
             </div>
           </div>
