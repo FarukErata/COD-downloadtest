1e4ea336103919e6ca64b70c321ff3b1d0576f34
Sefaciftci <75899474+Sefaciftci@users.noreply.github.com>
2024-08-15 09:46:53 +0300

    footer copyright updated

--- download-app/src/App.jsx
+++ download-app/src/App.jsx
@@ -10,13 +10,15 @@ function App() {
         <div className='content-box'>
           <div className='img-div'>
             <img className='icon' src={ainosLogo}></img>
-            <h3>Ainos Object Detection <span className='span'>V1.1</span> </h3>
+            <h3 className='content-title'>Ainos Object Detection <span className='span'>V1.1</span> </h3>
             <div className='btn-div' >
               <a href='https://drive.google.com/uc?export=download&id=1NMHntBgm50ckSh95RiMHHznAHHU2Jsqi
 ' className='download-btn'>Download</a>
             </div>
           </div>
         </div>
+
+        <div className='footer-div'>Copyrigth © 2024  |  <span className='footer-title'>AI</span>NOS</div>
 
 
       </div>
