cdc261e8c4b5b62b49c7e20278daf63ea5565a33
Sefaciftci <75899474+Sefaciftci@users.noreply.github.com>
2024-08-15 16:44:08 +0300

    verison update

--- download-app/src/App.jsx
+++ download-app/src/App.jsx
@@ -1,18 +1,17 @@
 import './App.css'
 import ainosLogo from './assets/ainosLogo2.jpeg';
+//https://drive.google.com/file/d/16M3mm76xj81kPwCuUDXU3CUjzFtaXaot/view?usp=sharing
 function App() {
 
   return (
     <>
       <div className='container'>
-
-
         <div className='content-box'>
           <div className='img-div'>
             <img className='icon' src={ainosLogo}></img>
             <h3 className='content-title'>Ainos Object Detection <span className='span'>V0.2.2</span> </h3>
             <div className='btn-div' >
-              <a href='https://drive.google.com/uc?export=download&id=1NMHntBgm50ckSh95RiMHHznAHHU2Jsqi
+              <a href='https://drive.google.com/uc?export=download&id=16M3mm76xj81kPwCuUDXU3CUjzFtaXaot
 ' className='download-btn'>Download</a>
             </div>
           </div>
